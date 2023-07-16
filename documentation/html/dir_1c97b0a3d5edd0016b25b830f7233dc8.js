e455d748eb1ae5982a4ef89614b612ec4a5544e1
Daniel Geiger <daniel.y.j.geiger@gmx.de>
2023-07-16 12:49:10 +0200

    doxygen files added

--- documentation/html/dir_1c97b0a3d5edd0016b25b830f7233dc8.js
+++
@@ -1,4 +0,0 @@
-var dir_1c97b0a3d5edd0016b25b830f7233dc8 =
-[
-    [ "CMakeCXXCompilerId.cpp", "_c_make_c_x_x_compiler_id_8cpp.html", "_c_make_c_x_x_compiler_id_8cpp" ]
-];
--- documentation/html/search/files_5.js
+++
@@ -1,9 +0,0 @@
-var searchData=
-[
-  ['socket_2ecpp_0',['Socket.cpp',['../_socket_8cpp.html',1,'']]],
-  ['socket_2ecpp_2eobj_2ed_1',['Socket.cpp.obj.d',['../_socket_8cpp_8obj_8d.html',1,'']]],
-  ['socket_2eh_2',['Socket.h',['../_socket_8h.html',1,'']]],
-  ['socketclient_2ecpp_3',['SocketClient.cpp',['../_socket_client_8cpp.html',1,'']]],
-  ['socketclient_2ecpp_2eobj_2ed_4',['SocketClient.cpp.obj.d',['../_socket_client_8cpp_8obj_8d.html',1,'']]],
-  ['socketclient_2eh_5',['SocketClient.h',['../_socket_client_8h.html',1,'']]]
-];
